d44952a84c21cc818efab852bbcbb9e1d95e3d45
Vinícius Oliveira de Carvalho <viniolicar2004@gmail.com>
2023-05-05 10:54:18 -0300

    curriculo pronto

--- index.js
+++
@@ -1,9 +0,0 @@
-const localstorage = localStorage;
-// const email = document.getElementById("email");
-
-// document.addEventListener("DOMContentLoaded", () => {
-//   //   email.innerHTML = localstorage["teste"];
-//   if (localStorage["teste"] === undefined) {
-//     window.location.href = "http://127.0.0.1:5500/form/form.html?"
-//   }
-// });
